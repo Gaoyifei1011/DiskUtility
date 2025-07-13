88e7d9cb7b8e16e709bbd2178add80fac5c16153
高怡飞 <363301617@qq.com>
2025-07-13 23:16:04 +0800

    First initialize DiskUtility project
    First initialize DiskUtility project

---
+++ DiskUtility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DiskUtility
+{
+    /// <summary>
+    /// 磁盘工具桌面程序
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点
+        /// </summary>
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
