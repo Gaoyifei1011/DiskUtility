d1b33c3f75dbfb0ca4e052a787b55ada47ed16ba
怡飞 高 <363301617@qq.com>
2025-07-27 00:07:49 +0800

    Add ResourceService
    Add ResourceService

---
+++ DiskUtility/Services/Root/ResourceService.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Resources;
+
+namespace DiskUtility.Services.Root
+{
+    /// <summary>
+    /// 应用资源服务
+    /// </summary>
+    public static class ResourceService
+    {
+        private static Assembly CurrentAssembly { get; } = Assembly.GetExecutingAssembly();
+
+        public static ResourceManager WindowResource { get; } = new("DiskUtility.Strings.Window", CurrentAssembly);
+    }
+}
